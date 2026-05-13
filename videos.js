e3dc6f8ea7eecc10d3cf9c744bcd50517bf1c0a6
lareenst24-sys <lareenst24@gmail.com>
2026-05-14 02:52:45 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,12 +1,45 @@
 window.siteVideos = [
-  // Videos you want everyone to see go here later.
-
-  // Example:
-  // {
-  //   id: "video-1",
-  //   title: "Example Video",
-  //   embed: "https://example.com/embed/123",
-  //   thumbnail: "https://example.com/thumb.jpg",
-  //   duration: "12:45"
-  // }
+  {
+    "id": "video-1778707057314-0",
+    "title": "",
+    "embed": "https://www.pornhub.com/embed/c3dbc9a5d726288d8a4b",
+    "thumbnail": "https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)5.jpg",
+    "duration": ""
+  },
+  {
+    "id": "video-1778707057315-1",
+    "title": "",
+    "embed": "https://www.pornhub.com/embed/f943adac0381c880e0ad",
+    "thumbnail": "https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)5.jpg",
+    "duration": ""
+  },
+  {
+    "id": "video-1778707057315-2",
+    "title": "",
+    "embed": "https://www.pornhub.com/embed/f8198bf83455b4e70464",
+    "thumbnail": "https://ei.phncdn.com/videos/201010/27/76481/original/(m=eaf8GgaaaWavb)(mh=AF1OIEHB-gGxbsoi)12.jpg",
+    "duration": ""
+  },
+  {
+    "id": "video-1778707057315-3",
+    "title": "",
+    "embed": "https://www.pornhub.com/embed/0a308ae49c7def7d967b",
+    "thumbnail": "https://ei.phncdn.com/videos/201010/27/77114/original/(m=eaf8GgaaaWavb)(mh=21Xbm0eckDZ2S7b9)15.jpg",
+    "duration": ""
+  },
+  {
+    "id": "video-1778707057316-4",
+    "title": "",
+    "embed": "https://www.pornhub.com/embed/5f8da82fd0c2a81f19cb",
+    "thumbnail": "https://ei.phncdn.com/videos/201010/27/77408/original/(m=eaf8GgaaaWavb)(mh=GNhXp4BRaIKcK9RB)6.jpg",
+    "duration": ""
+  },
+  {
+    "id": "video-1778707057316-5",
+    "title": "",
+    "embed": "https://www.pornhub.com/embed/226bf71ddc294e21de77",
+    "thumbnail": "https://ei.phncdn.com/videos/201010/27/77410/original/(m=eaf8GgaaaWavb)(mh=5OdWlhoMz1rni6xa)11.jpg",
+    "duration": ""
+  }
 ];
+
