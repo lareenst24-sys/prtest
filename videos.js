232bdf0223012876a0178c0a96efae94ca27f729
lareenst24-sys <lareenst24@gmail.com>
2026-05-14 02:30:14 +0530

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,12 @@
+const siteVideos = [
+  // Your public videos will go here.
+
+  // Example:
+  // {
+  //   id: "video-1",
+  //   title: "Example Video",
+  //   embed: "https://example.com/embed/123",
+  //   thumbnail: "https://example.com/thumb.jpg",
+  //   duration: "12:45"
+  // }
+];
